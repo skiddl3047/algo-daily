a1b9278431e7982e2ecc3a80e4a77601e50681ea
lkolla447 <46097644+lkolla447@users.noreply.github.com>
2025-02-21 18:00:18 -0500

    Update RotateByKPlaces.java

--- src/main/java/basics/RotateByKPlaces.java
+++ src/main/java/basics/RotateByKPlaces.java
@@ -2,20 +2,33 @@ package basics;
 
 import java.util.Arrays;
 
-public class LeftRotateByKPlaces {
+public class RotateByKPlaces {
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        new LeftRotateByKPlaces().leftRotateByKPlaces(nums, 15);
+        new RotateByKPlaces().leftRotateByKPlaces(nums, 1);
+        new RotateByKPlaces().rightRotateByKPlaces(nums, 1);
     }
 
-    private void leftRotateByKPlaces(int[] nums, int k) {
+    private void rightRotateByKPlaces(int[] nums, int k) {
+
         int length = nums.length;
         k = k%length;
 
         reverse(nums, 0, length-1);
         reverse(nums, 0, k-1);
         reverse(nums, k, length-1);
+
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private void leftRotateByKPlaces(int[] nums, int k) {
+        int length = nums.length;
+        k = k%length;
+
+        reverse(nums, 0, k-1);
+        reverse(nums, k, length-1);
+        reverse(nums, 0, length-1);
 
         System.out.println(Arrays.toString(nums));
     }
